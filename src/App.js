9a093cd9c5b71bae71d1dbed7fdb859c6c467522
Samantha Jeet <samanthajeet@gmail.com>
2019-05-04 18:28:45 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import styled from 'styled-components'
 import routes from './routes'
 
 import './App.css';
 
+const AppContainer = styled.div`
+box-sizing: border-box;
+  border: 2px solid green;
+  width: 95vw;
+  height: 95vh;
+  margin: auto;
+  padding: 10%;
+
+`
+
 function App() {
   return (
-    <>
+    <AppContainer>
       {routes}
-    </>
+    </AppContainer>
   );
   }
 
